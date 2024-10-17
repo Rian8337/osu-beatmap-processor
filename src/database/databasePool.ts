852119d7f3e087ddbc565efdbbb032ac3a106737
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-10-17 08:19:01 +0800

    Fix wrong pool connection configuration

--- src/database/databasePool.ts
+++ src/database/databasePool.ts
@@ -7,9 +7,9 @@ config();
  * The pool of connections to the database.
  */
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT!),
+    user: process.env.POSTGRES_USERNAME,
+    host: process.env.POSTGRES_HOSTNAME,
+    database: process.env.POSTGRES_DB_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT ?? "5432"),
 });
